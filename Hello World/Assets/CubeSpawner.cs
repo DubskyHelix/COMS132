58d069d03ea8ab90bae8563fa894b7d5d7c0ad05
DubskyHelix <jmccue925@gmail.com>
2019-09-10 14:21:00 -0400

    Second commit

---
+++ Hello World/Assets/CubeSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour
+{
+    public GameObject cubePrefabVar;
+
+
+
+    void Start()
+    {
+        Instantiate (cubePrefabVar);
+
+
+    }
+
+    
+    void Update()
+    {
+        Instantiate(cubePrefabVar);
+    }
+}
