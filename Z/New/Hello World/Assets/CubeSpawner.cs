13d4ce387894a62918eca34515a93c3321d968a9
DubskyHelix <jmccue925@gmail.com>
2019-09-17 15:56:21 -0400

    add

---
+++ Z/New/Hello World/Assets/CubeSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour
+{
+    public GameObject cubePrefabVar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+<<<<<<< HEAD
+        Instantiate(cubePrefabVar);
+=======
+        //Instantiate(cubePrefabVar);
+>>>>>>> f7df6143a1875f02efdccac4b6e069034ede29d9
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Instantiate(cubePrefabVar);
+    }
+}
