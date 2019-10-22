4ca5fb81807dfc5fb645d9a48dcf090e75991867
DubskyHelix <jmccue925@gmail.com>
2019-10-22 15:55:54 -0400

    Mission

--- Mission Demolition Prototype/Assets/__Scripts/Slingshot.cs
+++ Mission Demolition Prototype/Assets/__Scripts/Slingshot.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Slingshot : MonoBehaviour
 {
+    static private Slingshot S;
     [Header("Set in Inspector")]
     public GameObject prefabProjectile;
     public float velocityMult = 8f;
@@ -15,9 +16,17 @@ public class Slingshot : MonoBehaviour
     public GameObject projectile;
     public bool aimingMode;
     private Rigidbody projectileRigidbody;
+    static public Vector3 LAUNCH_POS
+    {
+        get {
+            if (S == null) return Vector3.zero;
+            return S.launchPos;
+        }
+    }
 
     void Awake()
     {
+        S = this;
         Transform launchPointTrans = transform.Find("LaunchPoint");
         launchPoint = launchPointTrans.gameObject;
         launchPoint.SetActive(false);
@@ -69,6 +78,8 @@ public class Slingshot : MonoBehaviour
             projectileRigidbody.velocity = -mouseDelta * velocityMult;
             FollowCam.POI = projectile;
             projectile = null;
+            MissionDemolition.ShotFired();
+            ProjectileLine.S.poi = projectile;
         }
     }
 }
